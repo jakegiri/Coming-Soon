0782cb5f17351c5f000eb5debde389c4c503a3f8
Jake Giri <neelunstp@gmail.com>
2021-10-10 04:53:57 +0530

    change server port number

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ var app = express();
 
 app.use(express.static(__dirname + "/public")); //__dir and not _dir
 
-app.listen(8000, () => console.log("server started"));
+app.listen(443, () => console.log("server started"));
